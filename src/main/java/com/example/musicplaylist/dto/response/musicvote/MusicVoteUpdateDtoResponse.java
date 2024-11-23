ddfa23990ea0ab4073e33f40afda1bef9ed7a201
skorea6 <skorea6@naver.com>
2024-11-23 23:53:51 +0900

    MusicVoteUpdateDtoResponse - status를 Boolean으로 변경

--- src/main/java/com/example/musicplaylist/dto/response/musicvote/MusicVoteUpdateDtoResponse.java
+++ src/main/java/com/example/musicplaylist/dto/response/musicvote/MusicVoteUpdateDtoResponse.java
@@ -6,11 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class MusicVoteUpdateDtoResponse {
+    private String memberUserId;
+    private String memberNick;
     private String type;
-    private boolean status;
+    private Boolean status;
     private MusicVoteInfoDtoResponse info;
 
-    public MusicVoteUpdateDtoResponse(String type, boolean status, MusicVoteInfoDtoResponse info) {
+    public MusicVoteUpdateDtoResponse(String memberUserId, String memberNick, String type, Boolean status, MusicVoteInfoDtoResponse info) {
+        this.memberUserId = memberUserId;
+        this.memberNick = memberNick;
         this.type = type;
         this.status = status;
         this.info = info;
